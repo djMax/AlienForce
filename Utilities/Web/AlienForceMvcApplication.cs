2e8125fcf63075893882f56ac0f3dc97e9da6f6b
Max Metral <max@pyralis.com>
2010-05-10 10:56:43 -0400

    Conditional authorization and https attributes for dev/prod environments

--- Utilities/Web/AlienForceMvcApplication.cs
+++ Utilities/Web/AlienForceMvcApplication.cs
@@ -9,26 +9,70 @@ using AlienForce.Utilities.DataAnnotations;
 using AlienForce.Utilities.DataAnnotations.Resources;
 using AlienForce.Utilities.Billing;
 using System.ComponentModel;
+using AlienForce.Utilities.Logging;
+using System.Configuration;
 
 namespace AlienForce.Utilities.Web
 {
 	public class AlienForceMvcApplication : System.Web.HttpApplication
 	{
+		static ILog Log = LogFramework.Framework.GetLogger(typeof(AlienForceMvcApplication));
+
+		private static Dictionary<string, bool> _EnvironmentTags = new Dictionary<string, bool>(StringComparer.OrdinalIgnoreCase);
+		private static bool _DebugEnv;
+
+		/// <summary>
+		/// Environment tags can be used for things like 'DEBUG' or 'NOSSL' that control major decisions
+		/// about the way the site should operate. This allows attributes to control whether SSL is required,
+		/// roles are enforced, and etc.  The tags are pulled from the app setting "AlienForce.EnvironmentTags"
+		/// and should be separated by commas.  Tags are case-insensitive.
+		/// </summary>
+		/// <param name="tag"></param>
+		/// <returns></returns>
+		public virtual bool HasEnvironmentTag(string tag)
+		{
+			if (String.Equals(tag, "DEBUG", StringComparison.OrdinalIgnoreCase))
+			{
+				return _DebugEnv;
+			}
+			return _EnvironmentTags.ContainsKey(tag);
+		}
+
 		public virtual void RegisterRoutes(RouteCollection routes)
 		{
-			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-			routes.MapRoutes(Assembly.GetCallingAssembly());
+			RegisterRoutes(routes, Assembly.GetCallingAssembly());
 		}
 
 		public virtual void RegisterRoutes(RouteCollection routes, Assembly assembly)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.IgnoreRoute("favicon.ico");
 			routes.MapRoutes(assembly);
 		}
 
 		protected virtual void Application_Start()
 		{
 			Logging.LogFramework.Framework.Initialize();
+
+			string tags = ConfigurationManager.AppSettings["AlienForce.EnvironmentTags"];
+			if (!String.IsNullOrWhiteSpace(tags))
+			{
+				string[] tagSplit = tags.Split(',');
+				foreach (string s in tagSplit)
+				{
+					string tr = s.Trim();
+					if (String.Equals(tr, "DEBUG", StringComparison.OrdinalIgnoreCase))
+					{
+						_DebugEnv = true;
+					}
+					_EnvironmentTags[tr] = true;
+				}
+				Log.InfoFormat("Application_Start beginning with environment tags: '{0}'.", tags);
+			}
+			else
+			{
+				Log.Info("Application_Start beginning (no environment tags set).");
+			}
 			AreaRegistration.RegisterAllAreas();
 			RegisterRoutes(RouteTable.Routes);
 
@@ -39,7 +83,6 @@ namespace AlienForce.Utilities.Web
 
 			System.ComponentModel.TypeDescriptor.AddAttributes(typeof(CreditCardNumber), new TypeConverterAttribute(typeof(CreditCardNumberTypeConverter)));
 			System.ComponentModel.TypeDescriptor.AddAttributes(typeof(ExpirationDate), new TypeConverterAttribute(typeof(ExpirationDateTypeConverter)));
-
 		}
 	}
 }
---
+++ Utilities/Web/EnvironmentRequireHttpsAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace AlienForce.Utilities.Web
+{
+	public class EnvironmentRequireHttpsAttribute : System.Web.Mvc.RequireHttpsAttribute
+	{
+		public string In  { get; set; }
+		public string NotIn { get; set; }
+
+		public override void OnAuthorization(System.Web.Mvc.AuthorizationContext filterContext)
+		{
+			if (String.IsNullOrWhiteSpace(In) && String.IsNullOrWhiteSpace(NotIn))
+			{
+				throw new ArgumentException("The EnvironmentRequireHttps attribute must have In or NotIn set in order to function.");
+			}
+			var app = filterContext.HttpContext.ApplicationInstance as AlienForceMvcApplication;
+			if (app == null)
+			{
+				throw new InvalidCastException("Your application must derive from AlienForceMvcApplication to use the EnvironmentRequireHttps attribute.");
+			}
+			// If In is set, and Not In is not set or does not apply, go for it.
+			if (In != null && app.HasEnvironmentTag(In) && (NotIn == null || !app.HasEnvironmentTag(NotIn)))
+			{
+				base.OnAuthorization(filterContext);
+			}
+			// Otherwise, if NotIn is set, just check that.
+			else if (NotIn != null && !app.HasEnvironmentTag(NotIn))
+			{
+				base.OnAuthorization(filterContext);
+			}
+		}
+
+	}
+}
